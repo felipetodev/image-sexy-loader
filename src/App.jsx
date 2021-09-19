b018e3f05110caf2af6fee884540c86bf007bb8c
felipetodev <fe.ossandon.u@gmail.com>
2021-09-19 14:17:40 -0300

    [🎨]: Style changes and increase transition effect

--- src/App.jsx
+++ src/App.jsx
@@ -9,26 +9,31 @@ const styles = {
 
 const blurEffect = {
   filterOn: {
-    filter: 'grayscale(1) blur(12px)'
+    filter: 'grayscale(1) blur(42px)',
+    opacity: 0
   },
   filterOff: {
     filter: 'grayscale(0) blur(0)',
-    transition: 'filter .5s, transform .5s'
+    transition: 'filter 1.2s, transform 1.2s',
+    willChange: 'filter, transform',
+    opacity: 1
   }
 }
 
 const Image = ({ loading, onLoad, scale }) => (
-  <img
-    src='../tom.jpeg'
-    alt='sexy-image-loader'
-    width='350px'
-    height='350px'
-    style={loading 
-      ? { ...blurEffect.filterOn, ...(scale && { transform: 'scale(1.2)' }) } 
-      : { ...blurEffect.filterOff, ...(scale && { transform: 'scale(1)' }) }}
-    onLoad={() => onLoad(false)}
-    loading='lazy'
-  />
+  <div style={{ overflow: 'hidden' }}>
+    <img
+      src='../tom.jpeg'
+      alt='sexy-image-loader'
+      width='350px'
+      height='350px'
+      style={loading 
+        ? { ...blurEffect.filterOn, ...(scale && { transform: 'scale(1.5)' }) } 
+        : { ...blurEffect.filterOff, ...(scale && { transform: 'scale(1)' }) }}
+      onLoad={() => onLoad(false)}
+      loading='lazy'
+    />
+  </div>
 )
 
 export default function App() {
@@ -36,9 +41,7 @@ export default function App() {
   return (
     <div style={styles}>
       <div style={{ marginRight: '80px' }}>
-        <div style={{ overflow: 'hidden' }}>
-          <Image loading={isLoading} onLoad={setLoading} scale />
-        </div>
+        <Image loading={isLoading} onLoad={setLoading} scale />
         <h2>it looks</h2>
       </div>
       <div>
